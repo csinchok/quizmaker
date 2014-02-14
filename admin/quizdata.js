9b6dab4b5c71dda19f0c25e59a7865adac8c019d
Adam Wentz <awentz@theonion.com>
2014-02-14 14:57:50 -0600

    example quizdata

---
+++ admin/quizdata.js
@@ -0,0 +1,26 @@
+/*
+var quizSchema = {
+    "results": {
+        "resultId": {
+            "title": "It's an result.",
+            "description": "Some longer text about this result",
+            "image": "http://something.something",
+        }
+    },
+    "questions": [
+        {
+            "question": "What's the deal with airline food?",
+            "image": "http://foobar.com/foo.jpg",
+            "answers": [
+                {
+                    "answer": "Peanuts.",
+                    "results": [
+                        "resultId0", "resultId1", "resultIdN"
+                    ],
+                    "forceResult": "optionalResultIdX" 
+                }
+            ]
+        }
+    ]
+}
+*/

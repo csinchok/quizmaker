b0b002dcb9a5da03d1d24be45c48019c33550a03
Shawn Cook <scook@theonion.com>
2014-02-15 12:08:16 -0600

    some cases

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,21 +1,36 @@
-var quiz = $("#quiz"),
-	q = quiz.find("li:not(.goBack)"), // questions
-	a = q.find("dd"), // answers
-	gB = quiz.find(".goBack a"); // go back
+var quiz = 	$('#quiz'),
+	q = 	quiz.find('li:not(.goBack)'), // questions
+	a = 	q.find('dd'), // answers
+	gB = 	quiz.find('.goBack a'); // go back
 
+// Go back button
 gB.each(function(index){
 	$(this).click( function(e){
-		var $this = $(this),
-			p = $(this).parent(); // get goBack parent question
+
+		var $this = 	$(this),
+			p = 		$(this).parent(); // goBack parent question
+
 		e.preventDefault();
-		console.log('you clicked "go back"');
+		console.log('you clicked \'go back\'');
+
 	});
 });
 
+// On answer click
 a.each(function(index){
 	$(this).click( function(){
-		var $this = $(this),
-			a = $this.parent().find("dd"); // get other answers
-		console.log(a); // show answers in console for now
+
+		var $this = 	$(this),
+			p = 		$this.parents('li'), // parent question
+			a = 		p.find('dd'); // other answers
+		
+		console.log($this); // log answer in console
+
+		if ( p.next("li").length ){
+			p.hide().next('li').show(); // hide question and show next
+		} else {
+			console.log("that was the last question") // indicate if last question
+		}
+
 	});
 });
